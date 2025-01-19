df37ab9b6fd0f32a26f6b0178a26f1157bf3cdbd
xuanthedev <nguyenxuanthe10082003@gmail.com>
2025-01-19 22:12:57 +0700

    #2: Doctor: UI Appbar, Sidebar & Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginForm from './pages/Auth/LoginForm'
+import Dashboard from './pages/Doctor/Dashboard/DashBoard'
 
 function App() {
   return (
     <Routes>
       {/* Authentication */}
       <Route path='/login' element={<LoginForm />} />
+
+      {/* Doctor Route */}
+      <Route path='/doctor/dashboard' element={<Dashboard />} />
     </Routes>
   )
 }
